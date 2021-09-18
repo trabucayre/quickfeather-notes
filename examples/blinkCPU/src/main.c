536304c8f3155188b498120dd39133b400d44835
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2021-09-18 16:42:03 +0200

    examples/blinkCPU: allows to select between quickfeather and qomu

--- examples/blinkCPU/src/main.c
+++ examples/blinkCPU/src/main.c
@@ -1,5 +1,11 @@
 #include "Fw_global_config.h" // This defines application specific characteristics
+#ifdef quickfeather
 #include "qf_hardwaresetup.h" // hardware init (clk / GPIOs)
+#define GREEN_PIN 6
+#else
+#include "qomu_hardwaresetup.h" // hardware init (clk / GPIOs)
+#define GREEN_PIN 0
+#endif
 
 #include "eoss3_hal_gpio.h"   // GPIO driver
 
@@ -9,12 +15,20 @@ void Delay(uint32_t nCount) {
 }
 
 int main(void) {
+#ifdef quickfeather
     qf_hardwareSetup();
+#else
+#ifdef qomu
+	qomu_hardwaresetup();
+#else
+#error "unknown board"
+#endif
+#endif
       
     while(1) {
-		HAL_GPIO_Write(6, 1);
+		HAL_GPIO_Write(GREEN_PIN, 1);
 		Delay(0x04FFFF);
-		HAL_GPIO_Write(6, 0);
+		HAL_GPIO_Write(GREEN_PIN, 0);
 		Delay(0x04FFFF);
 	}
 }
